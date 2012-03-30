b4cf030acad396cc8c4a55c0816ffde78d97a7e2
saasbook <saasbook@saasbook.(none)>
2012-03-30 07:57:41 -0700

    hw2 step4

---
+++ hw2_rottenpotatoes/app/models/movie.rb
@@ -0,0 +1,16 @@
+class Movie < ActiveRecord::Base
+  
+#  include Comparable
+#  def <=>(other)
+#    self.release_date <=> other.release_date
+#  end
+  def initialize()
+    
+  end
+  
+  @@all_ratings = ['G', 'PG', 'PG-13', 'R']
+  def self.all_ratings
+    @@all_ratings
+    
+  end
+end
